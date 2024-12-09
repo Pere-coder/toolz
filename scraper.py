d55c1d35ccf576223726a14e38ce0e8ca98fec94
Pere-coder <gpere800@gmail.com>
2024-12-09 03:24:36 +0100

    edits

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-
 def scrape_jumia(param):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
     }
     url = f"https://www.jumia.com.ng/catalog/?q={param}"
     response = requests.get(url, headers=headers)
 
     if response.status_code == 403:
-        st.error("Access Denied! The server rejected your request.")
+        print("Access Denied! The server rejected your request.")
         return []
 
     # Parse the page using BeautifulSoup
